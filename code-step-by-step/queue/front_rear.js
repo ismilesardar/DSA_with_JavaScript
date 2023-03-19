d71520f4a9fbb56d76d3b1d4169921bf7e43498b
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-19 04:30:48 +0000

    Front and Rear operations Queue | isEmpty function add

--- code-step-by-step/queue/front_rear.js
+++ code-step-by-step/queue/front_rear.js
@@ -4,44 +4,52 @@ let queue = [];
 let crentSize = queue.length;
 let maxSize = 5;
 
-function enqueue(newItem){
-  if(crentSize>=maxSize){
+function enqueue(newItem) {
+  if (crentSize >= maxSize) {
     console.log('Queue is Full');
-  }else{
-    
+  } else {
+
     queue[crentSize] = newItem;
-  crentSize++;
+    crentSize++;
   }
 }
 
-function dequeue(){
-  if(crentSize>0){
-    for(let i=0; i<crentSize ; i++){
-    queue[i] = queue[i+1];
-  }
-  crentSize--;
-  queue.length = crentSize;
-  }else{
+function dequeue() {
+  if (isEmpty()) {
+    for (let i = 0; i < crentSize; i++) {
+      queue[i] = queue[i + 1];
+    }
+    crentSize--;
+    queue.length = crentSize;
+  } else {
     console.log('Queue is empty')
   }
 }
 
-function display(){
+function display() {
   console.log(queue);
 }
 
-function font(){
-  if(crentSize>0){
+function font() {
+  if (isEmpty()) {
     console.log(queue[0]);
-  }else{
-    alert('Queue is Empty!');
+  } else {
+    console.warn('Queue is Empty!');
   }
 }
-function rear(){
-  if(crentSize>0){
-    console.log(queue[crentSize-1]);
-  }else{
-    alert('Queue is Empty!');
+function rear() {
+  if (isEmpty()) {
+    console.log(queue[crentSize - 1]);
+  } else {
+    console.warn('Queue is Empty!');
+  }
+}
+
+function isEmpty() {
+  if (crentSize <= 0) {
+    return false;
+  } else {
+    return true;
   }
 }
 
