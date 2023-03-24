8bafbf4ce8e31f80cd60b49fac9ec9d7ec503434
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-24 04:26:48 +0000

    Binary Search | while loop add

--- code-step-by-step/search/Binary_Search.js
+++ code-step-by-step/search/Binary_Search.js
@@ -4,4 +4,16 @@ let find = 44;
 let start = 0;
 let end = data.length - 1;
 let positions = undefined;
-
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+  // console.log(mid)
+  if (data[mid] === find) {
+    positions = mid;
+    break;
+  } else if (data[mid] < find) {
+    start = mid + 1;
+  } else {
+    start = mid - 1;
+  }
+}
+console.log(positions, data[positions])
