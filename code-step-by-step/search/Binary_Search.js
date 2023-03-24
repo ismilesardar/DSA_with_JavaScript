df4739c25730c9e6ac60935f3f069568a7ea5950
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-24 04:26:04 +0000

    Binary Search in JavaScript  start initial value

---
+++ code-step-by-step/search/Binary_Search.js
@@ -0,0 +1,7 @@
+// Binary Search in JavaScript 
+let data = [12, 14, 15, 22, 33, 44, 55, 66, 77, 88, 99, 101];
+let find = 44;
+let start = 0;
+let end = data.length - 1;
+let positions = undefined;
+
